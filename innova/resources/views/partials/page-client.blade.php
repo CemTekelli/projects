91246e41304df31dc3ce2221563dd96a27f3bc63
Cem Tekelli <tekelli.cem@outlook.com>
2022-03-13 21:47:56 +0100

    update

--- innova/resources/views/partials/page-client.blade.php
+++
@@ -1,8 +0,0 @@
-<div class="text-center">
-    <h1>PAGE CLIENT</h1>
-    <form method="POST" action="{{ route('logout') }}">
-        @csrf
-
-        <button class="btn btn-danger">deconnexion</button>
-    </form>
-</div>
--- innova/resources/views/template/contact/maps.blade.php
+++
@@ -1,4 +0,0 @@
-<div  class="d-none d-sm-block pb-4">
-    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2513.106326160842!2d4.227118115635554!3d50.95873837954868!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47c3eb41372e6097%3A0x40902b8c30988bbb!2sInnova%20Furniture!5e0!3m2!1sfr!2sbe!4v1641745476811!5m2!1sfr!2sbe" width="100%" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
-</div>
-<hr id="movingContact" style="margin: 50px 0">
